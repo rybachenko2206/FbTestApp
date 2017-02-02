045ff16bc519b36ccff153badc1d81ddc6cd9160
rrybachenko <rrybachenko@grossum.com>
2017-02-02 17:40:33 +0200

    initial commit

---
+++ FbTestApp/Classes/Controllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  FbTestApp
+//
+//  Created by Roman Rybachenko on 2/2/17.
+//  Copyright © 2017 Roman Rybachenko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
---
+++ FbTestApp/Classes/Extensions/UIStoryboard+Multiple.swift
@@ -0,0 +1,16 @@
+//
+//  UIStoryboard+Multiple.swift
+//
+//  Created by Roman Rybachenko on 11/1/16.
+//  Copyright © 2016 Roman Rybachenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIStoryboard {
+    static func main() -> UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: nil)
+    }
+}
